1e56605c2c1598af2462a34d66e24db9340ff115
GpsUkr <oleggpscom@gmail.com>
2022-12-18 01:59:25 +0200

    19

--- js/slider-customers.js
+++ js/slider-customers.js
@@ -1,8 +1,14 @@
 $(".customer_slider").slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    arrows: false,
-    slidesToScroll: 1
-  });
+  dots: true,
+  infinite: true,
+  speed: 2000,
+  slidesToShow: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pausOnDotHover: true,
+  pauseOnFocus: true,
+  pauseOnHover: true,
+  variableWidth: false,
+  slidesToScroll: 1
+});
